807bb256a4a271345baae996b85765ef526b8510
Angelica <angelicatanagras@gmail.com>
2026-04-14 09:26:51 +1000

    ci: update ip to trigger ci

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001',  //local development  
-  baseURL: 'http://3.27.160.54', // live
+  baseURL: 'http://54.206.84.82', // live
 });
 
 axiosInstance.interceptors.request.use((config) => {
