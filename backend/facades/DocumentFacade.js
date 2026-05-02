7efaa827075ef801101d31dfa7c74b1ba9e8490b
Angelica <angelicatanagras@gmail.com>
2026-05-02 20:29:29 +1000

    feat: add facade pattern between controller and services

---
+++ backend/facades/DocumentFacade.js
@@ -0,0 +1,45 @@
+const DocumentService = require('../services/DocumentService');
+
+class DocumentFacade {
+  constructor({ documentService = new DocumentService() } = {}) {
+    this.documentService = documentService;
+  }
+
+  uploadDocument(payload) {
+    return this.documentService.uploadDocument(payload);
+  }
+
+  listDocuments(payload) {
+    return this.documentService.getDocuments(payload);
+  }
+
+  listTrashedDocuments(userId) {
+    return this.documentService.getTrashedDocuments(userId);
+  }
+
+  getDocument(documentId, userId) {
+    return this.documentService.getDocument(documentId, userId);
+  }
+
+  updateDocument(payload) {
+    return this.documentService.updateDocument(payload);
+  }
+
+  moveDocumentToTrash(documentId, userId) {
+    return this.documentService.moveToTrash(documentId, userId);
+  }
+
+  restoreDocument(documentId, userId) {
+    return this.documentService.restoreDocument(documentId, userId);
+  }
+
+  permanentlyDeleteDocument(documentId, userId) {
+    return this.documentService.permanentlyDelete(documentId, userId);
+  }
+
+  prepareDownload(documentId, userId) {
+    return this.documentService.getDownloadPayload(documentId, userId);
+  }
+}
+
+module.exports = DocumentFacade;
